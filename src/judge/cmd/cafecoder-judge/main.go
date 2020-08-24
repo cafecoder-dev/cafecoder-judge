cda1e60a7eddaa59cf167946cd8282c4ee56edef
earlgray <earlgray283@gmail.com>
2020-08-24 15:30:30 +0900

    refactoring

---
+++ src/judge/cmd/cafecoder-judge/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/cafecoder-dev/cafecoder-judge/src/judge/types"
+	"github.com/cafecoder-dev/cafecoder-judge/src/judge/sqllib"
+	"github.com/cafecoder-dev/cafecoder-judge/src/judge/cmdlib"
+	"github.com/cafecoder-dev/cafecoder-judge/src/judge/judgelib"
+	"github.com/cafecoder-dev/cafecoder-judge/src/judge/util"
+	
+)
+
+func main() {
+	cmdChickets := types.CmdTicket{Channel: make(map[string]chan types.CmdResultJSON)}
+	go cmdlib.ManageCmds(&cmdChickets)
+
+	db, err := sqllib.NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		var res []types.SubmitsGORM
+
+		db.Table("submits").Where("status='WR' OR status='WJ'").Order("updated_at").Find(&res)
+
+		for i := 0; i < len(res); i++ {
+			cmdChickets.Lock()
+			_, exist := cmdChickets.Channel[fmt.Sprintf("%d", res[i].ID)]
+			cmdChickets.Unlock()
+
+			if exist {
+				continue
+			} else {
+				// wait until the number of judges becomes less than maxJudge
+				util.JudgeNumberLimit <- struct{}{}
+
+				cmdChickets.Lock()
+				cmdChickets.Channel[fmt.Sprintf("%d", res[i].ID)] = make(chan types.CmdResultJSON)
+				cmdChickets.Unlock()
+
+				go judgelib.Judge(res[i], &cmdChickets)
+			}
+		}
+	}
+}
+
---
+++ src/judge/types/gorm.go
@@ -0,0 +1,42 @@
+package types
+
+type ResultGORM struct {
+	Status          string `gorm:"column:status"`
+	ExecutionTime   int    `gorm:"column:execution_time"`
+	ExecutionMemory int    `gorm:"column:execution_memory"`
+	Point           int    `gorm:"column:point"` // int64 にしたほうがいいかもしれない(カラムにあわせて int にした)
+}
+
+type TestcaseResultsGORM struct {
+	SubmitID        int64  `gorm:"column:submit_id"`
+	TestcaseID      int64  `gorm:"column:testcase_id"`
+	Status          string `gorm:"column:status"`
+	ExecutionTime   int    `gorm:"column:execution_time"`
+	ExecutionMemory int    `gorm:"column:execution_memory"`
+	CreatedAt       string `gorm:"column:created_at"`
+	UpdatedAt       string `gorm:"column:updated_at"`
+}
+
+type TestcaseGORM struct {
+	TestcaseID int64  `gorm:"column:id"`
+	Input      string `gorm:"column:input"`
+	Output     string `gorm:"column:output"`
+}
+
+type SubmitsGORM struct {
+	ID        int64  `gorm:"column:id"`
+	Status    string `gorm:"column:status"`
+	ProblemID int64  `gorm:"column:problem_id"`
+	Path      string `gorm:"column:path"`
+	Lang      string `gorm:"column:lang"`
+}
+
+type TestcaseSetsGORM struct {
+	ID     int64 `gorm:"column:id"`
+	Points int   `gorm:"column:points"`
+}
+
+type TestcaseTestcaseSetsGORM struct {
+	TestcaseID    int64 `gorm:"column:testcase_id"`
+	TestcaseSetID int64 `gorm:"column:testcase_set_id"`
+}
