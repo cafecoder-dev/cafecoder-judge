908152d0534f16a3289a2069903a24ebfeee8614
earlgray <earlgray283@gmail.com>
2020-11-23 13:42:42 +0900

    内部名、一部コマンドを修正

--- src/langconf/langconf.go
+++ src/langconf/langconf.go
@@ -39,83 +39,83 @@ func LangConfig(langID string) (LanguageConfig, error) {
 	langConfig := LanguageConfig{}
 
 	switch langID {
-	case "c17_gcc10": //C17
+	case "c17_gcc:10.2.0": //C17
 		langConfig.CompileCmd = "gcc-10 Main.c -O2 -lm -std=gnu17 -o Main.out 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.out < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.c"
-	case "cpp17_gcc10": //C++17
+	case "cpp17_gcc:10.2.0": //C++17
 		langConfig.CompileCmd = "g++-10 Main.cpp -O2 -lm -std=gnu++17 -o Main.out 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.out < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.cpp"
-	case "cpp17_gcc10_acl": //C++17 + ACL
+	case "cpp17-acl_gcc:10.2.0": //C++17 + ACL
 		langConfig.CompileCmd = "g++-10 Main.cpp -O2 -lm -std=gnu++17 -I . -o Main.out 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.out < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.cpp"
-	case "cpp20_gcc10": //C++20
+	case "cpp20_gcc:10.2.0": //C++20
 		langConfig.CompileCmd = "g++-10 Main.cpp -O2 -lm -std=gnu++2a -o Main.out 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.out < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.cpp"
-	case "java11": //java11
+	case "java:11.0.9": //java11
 		langConfig.CompileCmd = "javac -encoding UTF-8 Main.java 2> userStderr.txt"
 		langConfig.ExecuteCmd = "java Main < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.java"
-	case "python38": //python3
-		langConfig.CompileCmd = "python3 -m py_compile Main.py 2> userStderr.txt"
-		langConfig.ExecuteCmd = "python3 Main.py < testcase.txt > userStdout.txt 2> userStderr.txt"
+	case "python:3.9.0": //python3
+		langConfig.CompileCmd = "python3.9 -m py_compile Main.py 2> userStderr.txt"
+		langConfig.ExecuteCmd = "python3.9 Main.py < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.py"
-	case "pypy3_73": //pypy3
+	case "pypy3:3.7.7": //pypy3
 		langConfig.CompileCmd = "pypy3 -m py_compile Main.py 2> userStderr.txt"
 		langConfig.ExecuteCmd = "pypy3 Main.py < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.py"
-	case "cs_mono6": //C#
+	case "cs_mono:6.12.0.90": //C#
 		langConfig.CompileCmd = "mcs Main.cs -out:Main.exe 2> userStderr.txt"
 		langConfig.ExecuteCmd = "mono Main.exe < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.cs"
-	case "cs_dotnet50": // C#
+	case "cs_dotnet:5.0": // C#
 		langConfig.CompileCmd = "cd Main && dotnet new console && mv ./../Main.cs Program.cs && dotnet publish -c Release --nologo -v q -o . 2> ../userStderr.txt && cd /"
 		langConfig.ExecuteCmd = "dotnet ./Main/Main.dll < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.cs"
-	case "go_115": //golang
+	case "go:1.15.5": //golang
 		langConfig.CompileCmd = "mv Main.go Main && cd Main && go build Main.go 2> ../userStderr.txt"
 		langConfig.ExecuteCmd = "./Main/Main < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.go"
-	case "nim_14":
+	case "nim:1.4.0":
 		langConfig.CompileCmd = "nim cpp -d:release --opt:speed --multimethods:on -o:Main.out Main.nim 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.out < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.nim"
-	case "rust_147":
+	case "rust:1.47.0":
 		langConfig.CompileCmd = "cd rust_workspace && mv /Main.rs ./src/main.rs && cargo build --release 2> /userStderr.txt && cd /"
 		langConfig.ExecuteCmd = "./rust_workspace/target/release/Rust < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.rs"
-	case "ruby_27":
+	case "ruby:2.7.2":
 		langConfig.CompileCmd = "ruby -w -c ./Main.rb 2> userStderr.txt"
 		langConfig.ExecuteCmd = "ruby ./Main.rb < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.rb"
-	case "kotlin_14":
+	case "kotlin:1.14.10":
 		langConfig.CompileCmd = "kotlinc ./Main.kt -include-runtime -d Main.jar 2> userStderr.txt"
 		langConfig.ExecuteCmd = "kotlin Main.jar < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.kt"
-	case "gfortlan_10":
-		langConfig.CompileCmd = "gfortran -O2 Main.f90 -o Main.out 2> userStderr.txt"
+	case "fortran:10.2.0":
+		langConfig.CompileCmd = "gfortran-10 -O2 Main.f90 -o Main.out 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.out < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.f90"
-	case "Perl5":
+	case "perl:5.30.3":
 		langConfig.CompileCmd = "perl -c Main.pl 2> userStderr.txt"
 		langConfig.ExecuteCmd = "perl Main.pl < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.pl"
-	case "Raku_2010":
+	case "raku:2020.09":
 		langConfig.CompileCmd = "perl6 -c Main.pl 2> userStderr.txt"
 		langConfig.ExecuteCmd = "perl6 Main.pl < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.p6"
-	case "crystal_035":
+	case "crystal:0.35.1":
 		langConfig.CompileCmd = "crystal build Main.cr 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.cr"
-	case "cat_8":
+	case "text_cat:8.30":
 		langConfig.CompileCmd = ":"
 		langConfig.ExecuteCmd = "cat Main.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.txt"
-	case "bash_5":
+	case "bash:5.0.17":
 		langConfig.CompileCmd = "bash -n Main.sh 2> userStderr.txt"
 		langConfig.ExecuteCmd = "./Main.sh < testcase.txt > userStdout.txt 2> userStderr.txt"
 		langConfig.FileName = "Main.sh"
